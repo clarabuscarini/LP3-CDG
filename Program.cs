603250360d222f9acd6165331586d00beff4d823
Julia Clara Buscarini <131157585+clarabuscarini@users.noreply.github.com>
2023-05-09 10:16:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using SP3072398ControleDeGastos;
+
+var conta1 = new Alimentacao("Vitoria", 0);
+
+conta1.EfetuarTransacao(20, DateTime.Now, "Café");
+conta1.EfetuarTransacao(50, DateTime.Now, "Compra de Mantimentos");
+conta1.ExecutarTransacaoFimdeMes();
+
+Console.WriteLine(conta1.ObterHistoricodeConta());
+
+var conta2 = new Vestuario("Vitoria", 0);
+
+conta2.EfetuarTransacao(100, DateTime.Now, "Agasalho");
+conta2.EfetuarTransacao(50, DateTime.Now, "Calça");
+conta2.EfetuarTransacao(250, DateTime.Now, "Terno");
+conta2.ExecutarTransacaoFimdeMes();
+
+Console.WriteLine(conta2.ObterHistoricodeConta());
+
+var conta3 = new Ingles("Vitoria", 0);
+
+conta3.EfetuarTransacao(200, DateTime.Now, "Cinema");
+conta3.ExecutarTransacaoFimdeMes();
+
+Console.WriteLine(conta3.ObterHistoricodeConta());
+
+var conta4 = new Futebol("Vitoria", 0);
+
+conta4.EfetuarTransacao(1000, DateTime.Now, "Linguagem C#");
+conta4.ExecutarTransacaoFimdeMes();
+
+Console.WriteLine(conta4.ObterHistoricodeConta());
